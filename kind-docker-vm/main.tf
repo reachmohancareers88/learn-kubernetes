7bf3226b93466393c779dce7aafb64dacbbb283b
Raghu K <raghuk.vit@gmail.com>
2026-05-22 06:16:01 +0530

    RoboShop Code

---
+++ kind-docker-vm/main.tf
@@ -0,0 +1,48 @@
+resource "azurerm_public_ip" "main" {
+  name                = var.vm_name
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vm_name}-nic"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+
+  ip_configuration {
+    name                          =  "${var.vm_name}-nic"
+    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = "${var.vm_name}-vm"
+  location              = "Denmark East"
+  resource_group_name   = "denmark-east-rg"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  size               = "Standard_B1s"
+
+  source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_password = "DevOps@123456"
+  admin_username = "devops"
+
+  disable_password_authentication = false
+
+  secure_boot_enabled = true
+  vtpm_enabled        = true
+
+}
+
+variable "vm_name" {
+  default = "docker"
+}
+
