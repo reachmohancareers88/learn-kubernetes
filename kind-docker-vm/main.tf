bb8d34131f23fca7812dc0742c445df3ed3d3348
Raghu K <raghuk.vit@gmail.com>
2026-05-28 23:33:45 +0530

    terraform: update 2 file(s) (18-roboshop-aks.yml,main.tf)

--- kind-docker-vm/main.tf
+++ kind-docker-vm/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_network_interface" "main" {
   resource_group_name = "denmark-east-rg"
 
   ip_configuration {
-    name                          =  "${var.vm_name}-nic"
+    name                          = "${var.vm_name}-nic"
     subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
@@ -31,7 +31,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   location              = "Denmark East"
   resource_group_name   = "denmark-east-rg"
   network_interface_ids = [azurerm_network_interface.main.id]
-  size               = "Standard_D2s_v3"
+  size                  = "Standard_D2s_v3"
 
   source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
 
@@ -64,10 +64,10 @@ resource "null_resource" "kind-setup" {
 
   provisioner "remote-exec" {
     connection {
-      host = azurerm_public_ip.main.ip_address
-      user = "devops"
+      host     = azurerm_public_ip.main.ip_address
+      user     = "devops"
       password = "DevOps@123456"
-      type = "ssh"
+      type     = "ssh"
     }
 
     inline = [
