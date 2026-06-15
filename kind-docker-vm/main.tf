80627d8a7ff0a637786d383340df16023a852e9c
Super User <root@Workstation.bwyvorfb4pkureq3qmo3dklkrc.cphx.internal.cloudapp.net>
2026-06-15 07:09:19 +0000

    Updated project for Mohan Azure environment

--- kind-docker-vm/main.tf
+++ kind-docker-vm/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "${var.vm_name}-nic"
-    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
+    subnet_id                     = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   network_interface_ids = [azurerm_network_interface.main.id]
   size                  = "Standard_D2s_v3"
 
-  source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+  source_image_id = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
 
   os_disk {
     caching              = "ReadWrite"
